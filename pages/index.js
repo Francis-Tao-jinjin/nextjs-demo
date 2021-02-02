d262da914f16a8122e21039dfdc7cacd7ffae6d2
taojinjin <taojinjin@codemao.cn>
2021-02-02 19:27:33 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head';
+import Layout, {siteTitle} from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link'
+import Date from '../components/date'
+
+// this fucntion only runs on the server-side.
+// It will never run on the client-side.
+// It won’t even be included in the JS bundle for the browser
+// Tt can only be exported from a page. 
+// You can’t export it from non-page files.
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    }
+  };
+}
+
+export default function Home({allPostsData}) {
+  console.log('allPostsData', allPostsData);
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I'm Francis Tao. I'm a software engineer, You can explore my GitHub for more infomation</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
